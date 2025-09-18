a8a1645bdad4721f54d6c68fca491838681b4928
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-09-19 03:14:55 +1000

    feat: new accent colours + authpage

---
+++ src/app/(public)/auth/page.tsx
@@ -0,0 +1,11 @@
+const authPage = () => {
+  return (
+    <>
+      <div className="min-h-svh flex justify-center items-center">
+        Login In | Sign Up
+      </div>
+    </>
+  )
+}
+
+export default authPage
